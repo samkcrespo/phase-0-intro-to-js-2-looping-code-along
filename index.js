35bffcbcac122b3edf6365674bf3789ca67a303f
Samantha Crespo <samkcrespo@gmail.com>
2022-04-05 22:52:32 -0700

    commit tree 0bfbc8929f6a6c3baa717ebb183ea882ebaa03be

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, newEvent) {
+    const thankYous = [];
+    for (let i = 0; i<names.length; i++){
+        thankYous.push(`Thank you, ${names[i]}, for the wonderful ${newEvent} gift!`)
+    }
+    return thankYous;
+}
+
+// writeCards(["Guadalupe", "Ollie", "Aki"], "Surprise");
+function countDown() {
+let i = 10;
+while (i >= 0) {
+    console.log(i--);
+}
+return countDown;
+} // Code your solutions in this file
